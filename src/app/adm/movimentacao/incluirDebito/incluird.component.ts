f84047d5c7b91d6a58328c47e2e7b2a1e70df7d3
Dantas <Dantas@DESKTOP-9R0DVMU>
2018-07-17 17:47:07 -0300

    ultimo

---
+++ src/app/adm/movimentacao/incluirDebito/incluird.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+
+import {MovimentacaoService} from '../../../services/movimentacao.service';
+import {ContaService} from '../../../services/conta.service';
+
+import {TbMovimentacao} from '../../../services/movimentacao';
+import {TbConta} from '../../../services/conta';
+
+import {Response} from '../../../services/response';
+
+import {Observable} from 'rxjs';
+
+
+@Component({
+  selector: 'app-incluird-movimentacao',
+  templateUrl: './incluird.component.html'
+})
+export class IncluirDMovimentacaoComponent implements OnInit {
+
+  private titulo: string;
+  private movimentacao: TbMovimentacao = new TbMovimentacao();
+  private contas: TbConta[] = new Array();
+
+  constructor(private movimentacaoService: MovimentacaoService,
+    private contaService: ContaService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {}
+
+
+  ngOnInit() {
+
+    this.contaService.getTbContas().subscribe(resp => this.contas = resp);
+
+    this.activatedRoute.params.subscribe(parametro => {
+
+        this.titulo = 'Inclusão de Débitos';
+      
+    });
+  }
+
+
+  salvar(): void {
+
+    this.movimentacaoService.addTbMovimentacao(this.movimentacao).subscribe(response => {
+
+      const res: Response = <Response>response;
+
+      if (res.codigo === 1) {
+        alert(res.mensagem);
+        this.movimentacao = new TbMovimentacao();
+        this.router.navigate(['/consulta-movimentacao']);
+      } else {
+
+        alert(res.mensagem);
+      }
+    },
+      (erro) => {
+        alert(erro);
+      });
+
+  }
+
+}
